6d467de83669aaa7b4c9e215a0d535c862a2f55e
Pawel Zun <pzun@unamo.com>
2018-11-25 23:26:40 +0100

    Get rid of useless coverage

--- test/components/Title.test.js
+++ test/components/Title.test.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import style from '../../src/components/Title.css';
 import Title from '../../src/components/Title'
 
 test('render a title', () => {
   const wrapper = shallow(
-      <Title 
-        headline="What are you going ToDo?"
-        howMany={4} />
+    <Title 
+      headline="What are you going ToDo?"
+      howMany={4} />
   );
   expect(wrapper).toMatchSnapshot();
 });
